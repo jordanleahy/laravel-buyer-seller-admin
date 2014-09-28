64b7ee4d77416a6b3b35c4e13c7c59c52dcaffc0
actkatiemacias <actkatiemacias@gmail.com>
2014-09-28 14:45:12 -0700

    added previous step

--- public/assets/javascript/user-signup.js
+++ public/assets/javascript/user-signup.js
@@ -1,18 +1,22 @@
 (function ($) {
     'use strict';
-    $(".datepicker").datetimepicker();
-    $( "#slider-range" ).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 75, 300 ],
-        slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-    $(document).ready(function () {
+    $(document).ready(function() {
+       if ($.ui) {
+           console.log("jqueryui loaded!");
+           $(".datepicker").datepicker();
+           $( "#slider-range" ).slider({
+               range: true,
+               min: 0,
+               max: 500,
+               values: [ 75, 300 ],
+               slide: function( event, ui ) {
+                   $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+               }
+           });
+           $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+               " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+       }
+
         var current_fs, next_fs, previous_fs; //fieldsets
         var left, opacity, scale; //fieldset properties which we will animate
         var animating; //flag to prevent quick multi-click glitches
@@ -51,6 +55,41 @@
             //this comes from the custom easing plugin
             easing: 'easeInOutBack'
         });**/
+        });
+
+        $(".previous").click(function(){
+            /**if(animating) return false;
+            animating = true;**/
+
+            current_fs = $(this).parent();
+            previous_fs = $(this).parent().prev();
+
+            //de-activate current step on progressbar
+            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+            //show the previous fieldset
+            previous_fs.show();
+            //hide the current fieldset with style
+            /**current_fs.animate({opacity: 0}, {
+                step: function(now, mx) {
+                    //as the opacity of current_fs reduces to 0 - stored in "now"
+                    //1. scale previous_fs from 80% to 100%
+                    scale = 0.8 + (1 - now) * 0.2;
+                    //2. take current_fs to the right(50%) - from 0%
+                    left = ((1-now) * 50)+"%";
+                    //3. increase opacity of previous_fs to 1 as it moves in
+                    opacity = 1 - now;
+                    current_fs.css({'left': left});
+                    previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+                },
+                duration: 800,
+                complete: function(){
+                    current_fs.hide();
+                    animating = false;
+                },
+                //this comes from the custom easing plugin
+                easing: 'easeInOutBack'
+            });**/
         });
 
 
